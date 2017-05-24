028a82b1cbf2fb84e7c8b360dffad1f9ae118c9c
Ben MacNeil <BenMacneil@Bens-Air.eastlink.ca>
2017-05-24 11:21:34 -0300

    Attempting to fix CORS errors

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+
 var config = require('./config');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+var items = require('./routes/items');
+var requests = require('./routes/requests');
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -15,13 +20,8 @@ mongoose.connect(config.database)
 	.then(() => console.log('connection successful'))
 	.catch((err) => console.log(err));
 	
-var index = require('./routes/index');
-var users = require('./routes/users');
-var items = require('./routes/items');
-var requests = require('./routes/requests');
 
-var app = express();
-
+const app = express();
 
 
 app.set('tokenSecret', config.secret);
@@ -30,36 +30,45 @@ app.set('tokenSecret', config.secret);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+// Headers
+app.use((err, req, res, next) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  
+
+});
+
+
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/items',items);
 app.use('/requests',requests);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
     'secret': 'tempestGroveIsMine',
-    'database': 'mongodb://localhost/the-merchants-guild-api'
+    'database': 'mongodb://localhost/the-merchants-guild-api',
+    CLIENT_SERVER_URL: 'http://localhost:3000'
 };
+
